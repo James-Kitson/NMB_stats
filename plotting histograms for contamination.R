756b3644eb0ff1151f5bc67f39b4cc5b1b67747a
James-Kitson <jameskitson10@gmail.com>
2015-11-20 09:35:23 +0000

    further code added

--- plotting histograms for contamination.R
+++ plotting histograms for contamination.R
@@ -1,5 +1,6 @@
 ###########################################################################
-###### Script for summarising MySeq reads into per well composition  #####
+###### Script for plotting the distributions of read depths and % reads for each assignment
+##### this is to look for potential cut-off values to clean my data
 ###########################################################################
 
 ### Clear the workspace
@@ -11,9 +12,17 @@ library(ggplot2)
 library(akima)
 library(plyr)
 library(dplyr)
-library(ggplot2bdc)
-library(ggExtra)
 library(grid)
 library(gridExtra)
 library(RColorBrewer)
 library(scales)
+
+### read in the data
+my.reads<-read.csv(file=paste("DATA/metaBEAT.tsv",sep=""), sep="\t", stringsAsFactors=FALSE, header=TRUE)
+
+### transpose the read data
+my.reads.trans<-recast(my.reads, variable~OTU_ID)
+colnames(my.reads.trans)[1]<-"sample"
+my.reads.trans$sample<-as.character(my.reads.trans$sample)
+
+### Plot
